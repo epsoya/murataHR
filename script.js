fd18396c08df8c948cd9567afdd11d65c6ec6d07
epsoya <144709923+epsoya@users.noreply.github.com>
2024-11-22 09:46:22 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function setViewportHeight() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+setViewportHeight();
+window.addEventListener('resize', setViewportHeight);
